7027b1e227483b2ac07a3daedb5d8701fff88afb
Davidson <dehpacifico@gmail.com>
2024-02-22 22:18:21 -0300

    Initial commit

---
+++ src/main/java/io/github/springsecurity/sbootexpsecurity/api/FooController.java
@@ -0,0 +1,20 @@
+package io.github.springsecurity.sbootexpsecurity.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController {
+
+  @GetMapping("/public")
+  public ResponseEntity<String> publicRoute () {
+    return ResponseEntity.ok("Public route ok.");
+  }
+
+  @GetMapping("/private")
+  public ResponseEntity<String> privateRoute() {
+    return ResponseEntity.ok("Private route ok.");
+  }
+}
