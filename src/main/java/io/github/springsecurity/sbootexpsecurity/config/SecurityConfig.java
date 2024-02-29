08a9826867b31b7a86b2f02fb2da756ccdca7c7f
Davidson <dehpacifico@gmail.com>
2024-02-28 21:20:42 -0300

    configurar autorizacao baseada em roles e autorities

--- src/main/java/io/github/springsecurity/sbootexpsecurity/config/SecurityConfig.java
+++ src/main/java/io/github/springsecurity/sbootexpsecurity/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,15 +21,19 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http,
-SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider) throws Exception {
+SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider,
+  CustomFilter customFilter
+  ) throws Exception {
     return http
             .authorizeHttpRequests(customizer -> {
               customizer.requestMatchers("/public").permitAll();
+              customizer.requestMatchers("/admin").hasRole("ADMIN");
               customizer.anyRequest().authenticated();
             })
             .httpBasic(Customizer.withDefaults())
             .formLogin(Customizer.withDefaults())
             .authenticationProvider(senhaMasterAuthenticationProvider)
+            .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
   }
 
@@ -52,5 +58,10 @@ SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider) throws Exce
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+    return new GrantedAuthorityDefaults("");
   }
 }
